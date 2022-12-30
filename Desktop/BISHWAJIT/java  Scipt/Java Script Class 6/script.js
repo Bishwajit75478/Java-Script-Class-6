013bb4cffbc0c6d6c432ca97e428668e9d23c457
bishwajit hazra <bishwajithazra75478@gmail.com>
2022-12-30 13:44:27 +0600

    first commit

---
+++ Desktop/BISHWAJIT/java  Scipt/Java Script Class 6/script.js
@@ -0,0 +1,124 @@
+// // function DECLARATIONS
+// function calculateAge(birthYear) {
+//   return 2022 - birthYear;
+// }
+
+// const age1 = calculateAge(2002);
+// console.log(age1);
+
+// // function Expressions
+// const calculateAgeNew = function (birthYear) {
+//   return 2022 - birthYear;
+// };
+
+// const age2 = calculateAgeNew(2000);
+// console.log(age2);
+
+// // function DECLARATIONS
+// const age1 = calculateAge(2002);
+// function calculateAge(birthYear) {
+//   return 2022 - birthYear;
+// }
+// console.log(age1);
+
+// // function Expressions
+// const calculateAgeNew = function (birthYear) {
+//   return 2022 - birthYear;
+// };
+
+// const age2 = calculateAgeNew(2000);
+// console.log(age2);
+
+// // Arro Function
+// // EXAMPLE 1
+// const calculateAgeAnother = (birthyear) => 2022 - birthyear;
+// const age1 = calculateAgeAnother(2002);
+// console.log(age1);
+
+// // example 2
+// const calculateAgeAnother1 = (birthyear) => {
+//   return 2022 - birthyear;
+// };
+// const age2 = calculateAgeAnother1(2002);
+// console.log(age2);
+
+// // good example 3
+// const jobLeft = (birthyear) => {
+//   const age = 2022 - birthyear;
+//   const ageLeft = 62 - age;
+//   return ageLeft;
+// };
+
+// const ageLeft = jobLeft(2002);
+// console.log(ageLeft);
+// console.log(`A JOB Age Left ${ageLeft} YEARS!`);
+
+// //good example 3
+// const jobLeft = (birthyear) => {
+//   const age = 2022 - birthyear;
+//   return 62 - age;
+// };
+
+// const ageLeft = jobLeft(2002);
+// console.log(ageLeft);
+// console.log(`A JOB Age Left ${ageLeft} YEARS!`);
+
+// // Array
+// const student1 = "ashif";
+// const student2 = "bappa";
+// const student3 = "bishwajit";
+
+// console.log(student1);
+// console.log(student2);
+// console.log(student3);
+
+// //Array EXAMPUL --1
+// const students = ["asif", "bappa", "bishwajit"];
+// console.log(students);
+// console.log(students[0]);
+// console.log(students[1]);
+
+// // array example 2
+// const player = new Array("SAKIB", "BAPPA", "BISHWAJIT");
+// console.log(player[0]);
+// console.log(player[1]);
+
+// const students = ["asif", "bappa", "bishwajit", "hazra"];
+// console.log(students);
+// students[0] = " karim";
+// console.log(students);
+
+// console.log(students.length);
+// console.log(students[students.length - 1]);
+
+// const user = ["bishwajit","hazra" 2002,2022-2002,"web develpoper"];
+// console.log (user);
+// console.log (user[ 1]);
+
+// // array methob
+// const users = ["user1", "user2", "user3"];
+// console.log(users);
+
+// // push
+// users.push("user4");
+// console.log(users);
+
+// // unshif
+// users.unshift("frist users");
+// console.log(users);
+
+// // pop
+// users.pop();
+// console.log(users);
+
+// //shift
+// users.shift();
+// console.log(users);
+
+// console.log(users);
+// const removeItame = users.pop();
+// console.log(users);
+
+// const removeItame = users.shift();
+// console.log(users);
+// console.log(removeItame);
